f1cf40ec64ba77a13fc38c758e2ad704b7aca60d
Cameron Moore <cmrnmr@gmail.com>
2020-02-28 13:50:13 -0500

    late start

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({username: ' ', password: ' '})
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+  }
+
+  const handleChanges = (e) => {
+    setCredentials({...credentials,
+      [e.target.name] : e.target.value
+    })
+  } 
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
